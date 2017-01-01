f99a62eec1072f16d44277f3a66fd92d57e88c67
Adam <adam@onlyzero.net>
2017-01-01 17:48:25 +0800

    Change to real blog URI

--- config.inc.php
+++ config.inc.php
@@ -1,5 +1,5 @@
 <?php
 $RSS_URI = 'https://onlyzero.net/Keyakizaka46_Blog_RSS';
 $XML_OUTPUT = 'Keyakizaka46-Blog.xml'; // Location of xml output
-$HTM_INPUT = 'blog.htm';
+$HTM_INPUT = 'http://www.keyakizaka46.com/mob/news/diarKiji.php?site=k46o&ima=0000&cd=member';
 ?>
