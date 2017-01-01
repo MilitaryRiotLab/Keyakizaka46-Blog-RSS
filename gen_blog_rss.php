7c8884fd9295b12693f11c25bfd003c967d59765
Adam <adam@onlyzero.net>
2017-01-01 20:00:13 +0800

    Optimize output and code
    * Remove unnecessary code and tag in RSS feed output
    * Less aggressive gzip compression

--- gen_blog_rss.php
+++ gen_blog_rss.php
@@ -49,7 +49,7 @@ for( $i = 0; $i <= 19; $i++ )
 
 	
 	$content = str_replace('src="','src="http://www.keyakizaka46.com',$content);
-	$array[$i]['content'] = htmlspecialchars( $content,  ENT_XML1, 'UTF-8');
+	$array[$i]['content'] = htmlspecialchars( strip_tags($content ,'<a><br><img><b>'),  ENT_XML1, 'UTF-8');
 }
 
 $date_now = date(DATE_RSS);
@@ -86,57 +86,8 @@ $output = $pre_loop.$loop.$post_loop;
 $xmlfile = fopen($root.$XML_OUTPUT, "w") or die("Unable to open file!");
 fwrite($xmlfile, $output);
 fclose($xmlfile);
-$output_gzip = gzencode($output,9);
+$output_gzip = gzencode($output,5);
 $xmlfile_gzip = fopen($root.$XML_OUTPUT.'.gz', "w") or die("Unable to open gzip file!");
 fwrite($xmlfile_gzip, $output_gzip);
 fclose($xmlfile_gzip);
 echo 'Jobs done';
-
-
-/*
-foreach($dom->find('h3') as $node) // Title
-{
-	$out .= strip_tags($dom->find('h3', $i) ,'<a>').'<br>';
-	$i ++;
-	
-}
-*/
-
-/*
-foreach($dom->find('article p.name') as $node) // Author
-{
-	$out .= strip_tags($dom->find('article p.name', $i) ,'<a>').'<br>';
-	$i ++;
-	
-}
-*/
-
-/*
-foreach($dom->find('div.box-article') as $node) // Content
-{
-    $out .= strip_tags($dom->find('div.box-article',$i) ,'<img><br><br /><div><b>')."==========";
-	$out = str_replace('</div>','<br>',$out);
-	//$out = str_replace('</b>','<br>',$out);
-	$out = str_replace('<div>','',$out);
-	$out = str_replace('<div id="AppleMailSignature">','',$out);
-	//$out = str_replace('<b>','',$out);
-	$i ++;
-}
-*/
-/*
-$i = 0;
-foreach($dom->find('div.box-bottom li') as $node) // Time / Data
-{
-		$out_time .= trim( strip_tags($dom->find('div.box-bottom li',$i) ), " ");
-		$i ++;
-}
-
-$time = str_replace('  ', '', explode( '個別ページ', $out_time ) );
-
-echo '<pre>';
-var_dump($array);
-echo '</pre>';
-
-echo $i.'<br>=============<br>';
-echo $out;
-*/
